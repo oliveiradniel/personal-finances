a915207f3a115ddfe658e36ca45b67760d5ee347
oliveiradniel <kadadniel@gmail.com>
2025-01-20 20:47:28 -0300

    chore: configure redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store.ts';
 
 import App from './App.tsx';
 
@@ -8,6 +11,8 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
