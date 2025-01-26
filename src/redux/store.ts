1e73fbbf149351f365a7fd30888c28427ef55c23
oliveiradniel <kadadniel@gmail.com>
2025-01-25 22:02:04 -0300

    feat: create the authSlice authentication slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+
+import authSlice from "./slices/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
