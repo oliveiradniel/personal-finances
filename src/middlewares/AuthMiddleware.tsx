33db0269ac40f99d4c9a79d714ce78e77e94d86f
oliveiradniel <kadadniel@gmail.com>
2025-02-14 05:02:44 -0300

    feat: add the authentication middleware

---
+++ src/middlewares/AuthMiddleware.tsx
@@ -0,0 +1,23 @@
+import { Navigate } from 'react-router-dom';
+
+import { useAppSelector } from '../redux/hooks';
+
+import Loader from '../components/Loader';
+
+type AuthMiddlewareProps = {
+  children: React.ReactNode;
+};
+
+export default function AuthMiddleware({ children }: AuthMiddlewareProps) {
+  const { authStatus } = useAppSelector((state) => state.auth);
+
+  if (authStatus === 'authenticated') {
+    return children;
+  }
+
+  if (authStatus === 'not_verified') {
+    return <Loader />;
+  }
+
+  return <Navigate to="/signin" />;
+}
