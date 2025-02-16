fa4542c0558f83039cec49768ca4cc70f84c9145
oliveiradniel <kadadniel@gmail.com>
2025-02-16 16:57:24 -0300

    feat: add the 'SelectInput' month and year component and its respective logic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,17 @@
+import { ChangeEvent, useState } from 'react';
+
 import { FcBullish, FcCancel, FcOk } from 'react-icons/fc';
 import { MdAdd } from 'react-icons/md';
 
 import Button from '../../components/Button';
+import SelectInput from '../../components/SelectInput';
 
 import {
   Body,
   BodyRow,
   Container,
   Header,
+  HeaderFilter,
   HeaderInfo,
   HeaderSubtitle,
   HeaderTitle,
@@ -20,6 +24,39 @@ import {
 } from './styles';
 
 export default function Dashboard() {
+  const [monthSelected, setMonthSelected] = useState(
+    (new Date().getMonth() + 1).toString().padStart(2, '0')
+  );
+  const [yearSelected, setYearSelected] = useState(
+    new Date().getFullYear().toString()
+  );
+
+  function handleMonthSelected({ target }: ChangeEvent<HTMLSelectElement>) {
+    setMonthSelected(target.value);
+  }
+
+  function handleYearSelected({ target }: ChangeEvent<HTMLSelectElement>) {
+    setYearSelected(target.value);
+  }
+
+  const yearsList = Array.from(
+    { length: new Date().getFullYear() - 2025 + 1 },
+    (_, index) => {
+      return {
+        value: (2025 + index).toString(),
+        label: (2025 + index).toString(),
+      };
+    }
+  );
+
+  const monthsList = Array.from({ length: 12 }, (_, index) => {
+    const date = new Date(new Date().getFullYear(), index, 1);
+    return {
+      value: (index + 1).toString().padStart(2, '0'),
+      label: date.toLocaleDateString('pt-BR', { month: 'long' }),
+    };
+  });
+
   return (
     <Container>
       <Header>
@@ -29,6 +66,20 @@ export default function Dashboard() {
             Acompanhe o seu saldo e filtre por mês e ano com facilidade.
           </HeaderSubtitle>
         </HeaderInfo>
+
+        <HeaderFilter>
+          <SelectInput
+            value={monthSelected}
+            options={monthsList}
+            onChange={handleMonthSelected}
+          />
+
+          <SelectInput
+            value={yearSelected}
+            options={yearsList}
+            onChange={handleYearSelected}
+          />
+        </HeaderFilter>
       </Header>
 
       <Body>
